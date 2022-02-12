dd7c00b583eac13b0ef4673b8e8c7ccca8892157
Peccosbill <jonathanmontini95@gmail.com>
2022-02-12 11:33:32 -0300

    Eliminando reducers no usados

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,16 +1,13 @@
 import {
   GET_COUNTRIES,
   GET_COUNTRY_ID,
-  GET_COUNTRY_BY_NAME,
-  RESET_COUNTRY
-  // GET_COUNTRY_BY_CONTINENT,
+  RESET_COUNTRY,
 } from "../actions/countriesActions";
 import { GET_ALL_ACTIVITIES } from "../actions/activityActions";
 
 const initialState = {
   countries: [],
   country: {},
-  countriesByContinent: [],
   activities: [],
 };
 
@@ -26,16 +23,6 @@ export default function rootReducer(state = initialState, action) {
         ...state,
         country: action.payload,
       };
-    case GET_COUNTRY_BY_NAME:
-      return {
-        ...state,
-        countries: action.payload,
-      };
-    // case ADD_ACTIVITY_IN_COUNTRIES:
-    //   return {
-    //     ...state,
-    //     activity: action.payload,
-    //   };
     case GET_ALL_ACTIVITIES:
       return {
         ...state,
