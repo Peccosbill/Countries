2c81bd8b28980af3498f9d265adc1db84e254244
Peccosbill <jonathanmontini95@gmail.com>
2022-02-18 12:44:12 -0300

    Agregar No hay actividades

--- client/src/components/AddActivity/AddActivity.jsx
+++ client/src/components/AddActivity/AddActivity.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import {
   addActivityInCountries,
